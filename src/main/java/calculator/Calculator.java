ddfc4dbeff3f256d1dcf35c5df2e311851fe6dd0
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2023-03-24 15:15:05 +0100

    create test class for testing

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,43 @@
+package calculator;
+
+public class Calculator {
+        //ATTRIBUTES
+        private float num1;
+        private float num2;
+
+
+    //Scopo di questa esercitazione è di implementare i metodi di una semplice calcolatrice e di
+            // scrivere una classe di test per verificare che funzionino come ci aspettiamo! Creare una classe chiamata Calculator avente i seguenti metodi pubblici :
+            //- float add(float num1, float num2)
+            //- float subtract(float num1, float num2)
+            //- float divide(float num1, float num2)
+            //- float multiply(float num1, float num2)
+            //Creare in seguito una classe di test che abbia 4 metodi, ognuno per verificare il corretto
+            // funzionamento di ogni metodo presente nella classe Calculator.
+
+        //METHODS
+        public float add(float num1, float num2){
+            return num1+num2;
+        }
+
+        public float subtract(float num1, float num2){
+            if(num1 >= num2){
+                return num1 - num2;
+            }else{
+                return num2 - num1;
+            }
+        }
+
+        public float divide(float num1, float num2){
+            if(num2 == 0){
+                throw new IllegalArgumentException("impossible divide per 0");
+            }else{
+                return num1/num2;
+            }
+        }
+
+        public float multiply(float num1, float num2){
+            return num1*num2;
+        }
+
+    }
